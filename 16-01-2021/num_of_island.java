cd1851ce31704b80f541d5acd63a4405f9f74cf2
19PA1A0548 <60499665+19PA1A0548@users.noreply.github.com>
2021-01-16 19:47:29 +0530

    Create num_of_island.java

---
+++ 16-01-2021/num_of_island.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int total_islands = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]== '1'){
+                    total_islands += dfs(grid,i,j);
+                }
+            }
+        }
+        return total_islands;
+    }
+    
+    public int dfs(char[][] grid,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0'){
+            return 0;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        return 1;
+    }
+}
