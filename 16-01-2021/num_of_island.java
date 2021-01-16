266f4c269e84a5b59e7862861497fdb5159f6ac8
19PA1A0548 <60499665+19PA1A0548@users.noreply.github.com>
2021-01-16 19:48:40 +0530

    Update num_of_island.java

--- 16-01-2021/num_of_island.java
+++ 16-01-2021/num_of_island.java
@@ -15,11 +15,13 @@ class Solution {
         if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0'){
             return 0;
         }
+        else{
         grid[i][j]='0';
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
         dfs(grid,i,j+1);
         dfs(grid,i,j-1);
         return 1;
+        }
     }
 }
